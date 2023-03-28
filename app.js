8bf986a25e966cbe14134546e7ecb8a16456bd52
nadir inab <nadir.inab.dev@gmail.com>
2023-03-28 09:27:14 +0000

    popular resource is well added

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ app.use(express.json());
 
 // In-memory array of flights data
 let flights = [
-  { 
-    id: 1, 
-    date: '2023-04-01', 
-    from: 'New York', 
-    to: 'London', 
+  {
+    id: 1,
+    date: '2023-04-01',
+    from: 'New York',
+    to: 'London',
     airport: 'JFK',
     airline: 'Delta',
     aircraft: 'Boeing 747',
@@ -17,13 +17,22 @@ let flights = [
     longitude: -73.7781,
     flight_status: 'On time',
     price: 1000,
-    seats: 200
+    seats: 200,
+    popular_destinatons: [
+      {
+        id: 1,
+        city: 'Roma',
+        image: 'https:\\test.com',
+        price: 1200
+      }
+    ]
+
   },
-  { 
-    id: 2, 
-    date: '2023-04-02', 
-    from: 'London', 
-    to: 'New York', 
+  {
+    id: 2,
+    date: '2023-04-02',
+    from: 'London',
+    to: 'New York',
     airport: 'LHR',
     airline: 'British Airways',
     aircraft: 'Boeing 787',
@@ -34,11 +43,11 @@ let flights = [
     price: 900,
     seats: 180
   },
-  { 
-    id: 3, 
-    date: '2023-04-03', 
-    from: 'San Francisco', 
-    to: 'Tokyo', 
+  {
+    id: 3,
+    date: '2023-04-03',
+    from: 'San Francisco',
+    to: 'Tokyo',
     airport: 'SFO',
     airline: 'United Airlines',
     aircraft: 'Airbus A350',
@@ -49,11 +58,11 @@ let flights = [
     price: 1500,
     seats: 250
   },
-  { 
-    id: 4, 
-    date: '2023-04-04', 
-    from: 'Wasignton DC', 
-    to: 'Rabat', 
+  {
+    id: 4,
+    date: '2023-04-04',
+    from: 'Wasignton DC',
+    to: 'Rabat',
     airport: 'M6',
     airline: 'Morroccan Airlines',
     aircraft: 'Airbus A350',
@@ -64,11 +73,11 @@ let flights = [
     price: 1500,
     seats: 250
   },
-  { 
-    id: 5, 
-    date: '2023-04-05', 
-    from: 'Algerie', 
-    to: 'Riad', 
+  {
+    id: 5,
+    date: '2023-04-05',
+    from: 'Algerie',
+    to: 'Riad',
     airport: 'SFO',
     airline: 'Saudi Airlines',
     aircraft: 'Airbus A350',
@@ -79,11 +88,11 @@ let flights = [
     price: 1500,
     seats: 250
   },
-  { 
-    id: 6, 
-    date: '2023-04-06', 
-    from: 'Ankara', 
-    to: 'Madrid', 
+  {
+    id: 6,
+    date: '2023-04-06',
+    from: 'Ankara',
+    to: 'Madrid',
     airport: 'SFO',
     airline: 'Turksh Airlines',
     aircraft: 'Airbus A350',
@@ -94,11 +103,11 @@ let flights = [
     price: 1500,
     seats: 250
   },
-  { 
-    id: 7, 
-    date: '2023-04-07', 
-    from: 'Cairo', 
-    to: 'Marrakech', 
+  {
+    id: 7,
+    date: '2023-04-07',
+    from: 'Cairo',
+    to: 'Marrakech',
     airport: 'SFO',
     airline: 'Egyptian Airlines',
     aircraft: 'Airbus A350',
@@ -109,11 +118,11 @@ let flights = [
     price: 1500,
     seats: 250
   },
-  { 
-    id: 8, 
-    date: '2023-04-08', 
-    from: 'San Francisco', 
-    to: 'Tokyo', 
+  {
+    id: 8,
+    date: '2023-04-08',
+    from: 'San Francisco',
+    to: 'Tokyo',
     airport: 'SFO',
     airline: 'United Airlines',
     aircraft: 'Airbus A350',
@@ -124,11 +133,11 @@ let flights = [
     price: 1500,
     seats: 160
   },
-  { 
-    id: 9, 
-    date: '2023-04-09', 
-    from: 'San Francisco', 
-    to: 'Roma', 
+  {
+    id: 9,
+    date: '2023-04-09',
+    from: 'San Francisco',
+    to: 'Roma',
     airport: 'SFO',
     airline: 'Italy Airlines',
     aircraft: 'Airbus A350',
@@ -139,11 +148,11 @@ let flights = [
     price: 800,
     seats: 100
   },
-  { 
-    id: 10, 
-    date: '2023-04-10', 
-    from: 'Agadir', 
-    to: 'Tanger', 
+  {
+    id: 10,
+    date: '2023-04-10',
+    from: 'Agadir',
+    to: 'Tanger',
     airport: 'SFO',
     airline: 'Arabic Airlines',
     aircraft: 'Airbus A350',
@@ -175,15 +184,28 @@ app.get('/flights/:from/:to', (req, res) => {
   res.json(filteredFlights);
 });
 
+app.get('/flights/price/:price', (req, res) => {
+  const { price } = req.params;
+  const filtredFlight = flights.filter(flight => flight.price === price);
+  res.json(filtredFlight);
+});
+
 // Route to add a new flight
 app.post('/flights', (req, res) => {
-  const newFlight = { 
+  const newFlight = {
     id: flights.length + 1,
-    ...req.body 
+    ...req.body
   };
   flights.push(newFlight);
   res.json(newFlight);
 });
+
+// Route to get all popular destinations : 
+app.get("/flights/popular", (req, res) => {
+  const popular = flights.map((flight) => {
+    return flight.popular;
+  })
+})
 
 app.listen(3000, () => {
   console.log('Flights API listening on port 3000');
